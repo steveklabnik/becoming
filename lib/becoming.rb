a1bc6ef7f62b5650441adf527b662002f190c495
Ryan Biesemeyer <ryan@simplymeasured.com>
2013-05-27 23:47:23 +0000

    allow multiple becomings on a single object; add unbecoming

--- lib/becoming.rb
+++ lib/becoming.rb
@@ -1,20 +1,26 @@
 require "becoming/version"
 
 module Becoming
-  def becoming(mod)
-    @becoming = mod    
+  def becoming(mod=nil)
+    @becoming ||= []
+    mod && @becoming |= [mod]
+    @becoming
+  end
+
+  def unbecoming(mod)
+    becoming.delete(mod)
   end
 
   def method_missing(m, *args, &blk)
-    if @becoming && @becoming.public_method_defined?(m)
-      @becoming.instance_method(m).bind(self).call(*args, &blk)
+    if match = becoming.find { |b| b.public_method_defined?(m) }
+      match.instance_method(m).bind(self).call(*args, &blk)
     else
       super
     end
   end
 
   def respond_to_missing?(m, include_all=false)
-    if @becoming && @becoming.public_method_defined?(m)
+    if becoming.any? { |b| b.public_method_defined?(m) }
       true
     else
       super
--- test/becoming_test.rb
+++ test/becoming_test.rb
@@ -12,11 +12,28 @@ module FullNamed
   end
 end
 
+module Nicknamed
+  def nickname
+    "#{first_name} \"SKRILLEX\" #{last_name}"
+  end
+end
+
 class BecomingTest < MiniTest::Unit::TestCase
   def test_becomings
     user = User.new("Steve", "Klabnik")
     user.becoming(FullNamed)
+    user.becoming(Nicknamed)
 
+    assert user.send(:respond_to_missing?, :full_name)
     assert_equal "Steve Klabnik", user.full_name
+    assert user.send(:respond_to_missing?, :nickname)
+    assert_equal 'Steve "SKRILLEX" Klabnik', user.nickname
+
+    user.unbecoming(Nicknamed)
+
+    assert !user.send(:respond_to_missing?, :nickname)
+    assert_raises(NoMethodError) { user.nickname }
+
+    assert user.send(:respond_to_missing?, :full_name)
   end
 end
