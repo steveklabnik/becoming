ea8c8d46eda51253e1f2150900a74ff845c9d740
Steve Klabnik <steve@steveklabnik.com>
2013-05-27 17:03:45 -0500

    Make it all work out.
    Much love to @avdi for this idea.

--- lib/becoming.rb
+++ lib/becoming.rb
@@ -1,5 +1,23 @@
 require "becoming/version"
 
 module Becoming
-  # Your code goes here...
+  def becoming(mod)
+    @becoming = mod    
+  end
+
+  def method_missing(m, *args, &blk)
+    if @becoming && @becoming.public_method_defined?(m)
+      @becoming.instance_method(m).bind(self).call(*args, &blk)
+    else
+      super
+    end
+  end
+
+  def respond_to_missing?(m, include_all=false)
+    if @becoming && @becoming.public_method_defined?(m)
+      true
+    else
+      super
+    end
+  end
 end
--- test/becoming_test.rb
+++ test/becoming_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
+require 'becoming'
+
+User = Struct.new(:first_name, :last_name) do
+  include Becoming
+end
+
+module FullNamed
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+end
+
 class BecomingTest < MiniTest::Unit::TestCase
-  def test_truth
-    assert_equal 0, 0
+  def test_becomings
+    user = User.new("Steve", "Klabnik")
+    user.becoming(FullNamed)
+
+    assert_equal "Steve Klabnik", user.full_name
   end
 end
